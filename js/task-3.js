2390c41511818b22a750c237576fda9f657c72c9
yakovleva2904 <yakovlevaolga2904@gmail.com>
2024-06-21 22:52:15 +0200

    HW is done

---
+++ js/task-3.js
@@ -0,0 +1,15 @@
+function filterArray(numbers, value) {
+  const tags = [];
+  for (let i = 0; i < numbers.length; i++){
+    if (numbers[i] > value) {
+      tags.push(numbers[i]);
+    }
+  }
+  return tags;
+}
+console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
+console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
+console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
+console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
+console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
+
